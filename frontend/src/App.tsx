12dfaa111600004b7846df62dad6ab83ae9e608c
Jakub Marciniak <jakub.marciniak2000@gmail.com>
2025-02-04 19:25:10 +0100

    created new react frontend application

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Login from './components/credentials/Login'
+import { Register } from './components/credentials/Register'
+
+function App() {
+  return (
+    <>
+      {/* <Login /> */}
+      <Register />
+    </>
+  )
+}
+
+export default App
